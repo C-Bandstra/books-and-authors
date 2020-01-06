3aae9ec6916946f1b9b30ec871e13ade924db55d
Charlie Bandstra <bandstracharlie@gmail.com>
2020-01-05 21:51:03 -0700

    finish assignment

--- author.js
+++ author.js
@@ -1,5 +1,15 @@
+var Book = require('./book.js');
+
 class Author {
-
+  constructor(authorInfo) {
+    this.first = authorInfo.first;
+    this.last = authorInfo.last;
+    this.books = [];
+  }
+  write(title, publicationYear) {
+    var newBook = new Book(this.first, this.last,
+       this.books.push(newBook));
+       return newBook;
+  }
 }
-
 module.exports = Author;
